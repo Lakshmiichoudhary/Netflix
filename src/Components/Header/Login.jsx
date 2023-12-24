332f4d580f994cd4f6268316f65cffa05ac19d78
lakshmiichoudhary <laxmi2252001@gmail.com>
2023-12-24 20:21:10 +0530

    added validation

--- src/Components/Header/Login.jsx
+++ src/Components/Header/Login.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Header from './Header'
 import image from '../../assets/image.png'
+import Validation from '../Validation'
 
 const Login = () => {
   const[isSingIn,setIsSignIn] = useState(false)
+  const [errorMessage,setErrorMessage] = useState(null)
+
+  const name = useRef(null)
+  const email = useRef(null)
+  const password = useRef(null)
+
+  const submitHandler = () => {
+    const message = Validation(name.current.value,email.current.value, password.current.value);
+    setErrorMessage(message);
+  }
 
   const toggle = () => {
     setIsSignIn(!isSingIn)
@@ -15,16 +26,31 @@ const Login = () => {
       <div className='absolute'>
         <img src={image} alt='background'/>
       </div>
-      <div className='w-4/12 absolute bg-black p-12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+      <div className='w-4/12 absolute bg-black p-11 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
         <h1 className='font-bold text-3xl p-4'>{isSingIn ? "Sign In" : "Sign Up"}</h1>
-        <form>
-          {!isSingIn && <input type='text' placeholder='Full Name' className='p-4 m-2 w-full
+        <form onSubmit={(e) => e.preventDefault()}>
+          {!isSingIn && 
+          <input 
+            ref={name}
+            type='text'
+            placeholder='Full Name' 
+            className='p-4 m-2 w-full
           bg-gray-700 rounded-md'/>}
-          <input type='text' placeholder='Email or phone Number' className='p-4 m-2 w-full
+          <input 
+            ref={email}
+            type='text' 
+            placeholder='Email or phone Number' 
+            className='p-4 m-2 w-full
           bg-gray-700 rounded-md'/>
-          <input type='text' placeholder='Password' className='p-4 m-2 w-full
+          <input 
+            ref={password} 
+            type='password' 
+            placeholder='Password' 
+            className='p-4 m-2 w-full
           bg-gray-700 rounded-md'/>
-          <button className='p-3 m-2 bg-red-700 w-full rounded-md'>{isSingIn ? "Sign In" : "Sign Up"}</button>
+          <p className='p-2 text-red-700 font-bold'>{errorMessage}</p>
+          <button className='p-3 m-2 bg-red-700 w-full rounded-md' onClick={submitHandler}>
+            {isSingIn ? "Sign In" : "Sign Up"}</button>
           <p className='p-2 cursor-pointer' onClick={toggle}>
             {isSingIn ? "New to Netflix? Sign up now" : "Existing User? Sing In"}</p>
         </form>
---
+++ src/Components/Validation.jsx
@@ -0,0 +1,12 @@
+const Validation = (name, email, password) => {
+
+    const emailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email);
+    const passwordValid = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password);
+    const namevalid = /^[a-zA-Z]+(?: [a-zA-Z]+)*$/.test(name);
+
+    if(!emailValid) return "Enter a valid email";
+    if(!passwordValid) return "Enter a valid password";
+    if(!namevalid) return "Enter a valid name";
+}
+
+export default Validation
